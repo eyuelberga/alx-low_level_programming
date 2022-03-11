92c92fb3979a451c4a8d7c6a55aef77852112c75
Eyuel Berga Woldemichael <4353169-eyuelberga@users.noreply.gitlab.com>
2022-03-11 19:29:33 +0300

    0x01

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all the numbers of base 16 in lowercase, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	char c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
