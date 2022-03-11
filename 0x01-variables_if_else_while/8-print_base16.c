cb64f5421a48032f779fc965d2974981bef203f2
Eyuel Berga Woldemichael <4353169-eyuelberga@users.noreply.gitlab.com>
2022-03-11 19:42:12 +0300

    fix betty styling

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 /**
  * main - main block
- * Description: prints all the numbers of base 16 in lowercase, followed by a new line.
+ * Description: prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
  * Return: 0
  */
 int main(void)
 {
   int i, y;
   for (i = 0; i < 10; i++)
-      {
-        putchar (i + '0');
-      }
+  {
+    putchar (i + '0');
+  }
+
   for (y = 'a'; y <= 'f'; y++)
-      {
-        putchar (y);
-      }
-	putchar('\n');
+  {
+    putchar (y);
+  }
+
+  putchar('\n');
 	return (0);
 }
